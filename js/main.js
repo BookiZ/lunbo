ae868903e2da7043a3f768bf9431ba6bf042df10
shuyan <326038581@qq.com>
2018-03-31 15:22:45 +0800

    first

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$('#p1').on('click',function () {
+    $('.images').css({
+        transform:'translateX(0)'
+
+    })
+
+})
+if($('#p1'))
+console.log($('#p1'))
+$('#p2').on('click',function () {
+    $('.images').css({
+        transform:'translateX(-300px)'
+
+    })
+
+})
+$('#p3').on('click',function () {
+    $('.images').css({
+        transform:'translateX(-600px)'
+
+    })
+
+})
